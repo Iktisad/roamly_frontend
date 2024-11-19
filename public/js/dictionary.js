bff2363aca1f305f33392ff20643e7e24c5328aa
Iktisad Rashid <iktisad.rashid@hotmail.com>
2024-11-19 04:21:45 -0500

    dashboard done

---
+++ public/js/dictionary.js
@@ -0,0 +1,157 @@
+export const weatherGifMap = {
+    Clear: {
+        day: "../public/icons/sun.gif",
+        night: "../public/icons/cloudy-night.gif",
+    },
+    Clouds: {
+        day: "../public/icons/clouds.gif",
+        night: "../public/icons/cloudy-night.gif",
+    },
+    Rain: {
+        day: "../public/icons/rain-day.gif",
+        night: "../public/icons/rain-night.gif",
+    },
+    Snow: {
+        day: "../public/icons/snow.gif",
+        night: "../public/icons/snow.gif",
+    },
+    Thunderstorm: {
+        day: "../public/icons/storm-day.gif",
+        night: "../public/icons/storm-night.gif",
+    },
+    Drizzle: {
+        day: "../public/icons/drizzle.gif",
+        night: "../public/icons/drizzle.gif",
+    },
+    Mist: {
+        day: "../public/icons/mist-day.gif",
+        night: "../public/icons/mist-night.gif",
+    },
+    Smoke: {
+        day: "../public/icons/smoke-day.gif",
+        night: "../public/icons/smoke-night.gif",
+    },
+    Haze: {
+        day: "../public/icons/haze-day.gif",
+        night: "../public/icons/haze-night.gif",
+    },
+    Dust: {
+        day: "../public/icons/dust-day.gif",
+        night: "../public/icons/dust-night.gif",
+    },
+    Fog: {
+        day: "../public/icons/foggy.gif",
+        night: "../public/icons/foggy.gif",
+    },
+    Sand: {
+        day: "../public/icons/sand-day.gif",
+        night: "../public/icons/sand-night.gif",
+    },
+    Ash: {
+        day: "../public/icons/ash-day.gif",
+        night: "../public/icons/ash-night.gif",
+    },
+    Squall: {
+        day: "../public/icons/squall-day.gif",
+        night: "../public/icons/squall-night.gif",
+    },
+    Tornado: {
+        day: "../public/icons/tornado-day.gif",
+        night: "../public/icons/tornado-night.gif",
+    },
+    Default: {
+        day: "../public/icons/temperature.gif",
+        night: "../public/icons/temperature.gif",
+    },
+};
+
+export const greetings = [
+    "Hope you're having a great day!",
+    "Welcome back! Ready to explore?",
+    "Here's to a productive session!",
+    "Your travel companion awaits!",
+    "Let's make today a great day!",
+    "Another day, another adventure!",
+];
+
+
+// Mock restaurant data
+export const suggestions = [
+    {
+        name: "Pasta Paradise",
+        distance: "1.2 km",
+        rating: 4.5,
+        reviews: 120,
+    },
+    {
+        name: "Burger Haven",
+        distance: "0.8 km",
+        rating: 4.7,
+        reviews: 98,
+    },
+    {
+        name: "Sushi World",
+        distance: "2.3 km",
+        rating: 4.3,
+        reviews: 140,
+    },
+    {
+        name: "Boustan",
+        distance: "600 m",
+        rating: 4.3,
+        reviews: 140,
+    },
+    {
+        name: "Kebab Express",
+        distance: "200 m",
+        rating: 4.3,
+        reviews: 140,
+    },
+    {
+        name: "Chef Istanbul",
+        distance: "2.3 km",
+        rating: 4.3,
+        reviews: 140,
+    },
+];
+
+
+// Mock restaurant data
+export const popularSuggestions = [
+    {
+        name: "Pasta Paradise",
+        distance: "1.2 km",
+        rating: 4.5,
+        reviews: 120,
+    },
+    {
+        name: "Burger Haven",
+        distance: "0.8 km",
+        rating: 4.7,
+        reviews: 98,
+    },
+    {
+        name: "Sushi World",
+        distance: "2.3 km",
+        rating: 4.3,
+        reviews: 140,
+    },
+    {
+        name: "Boustan",
+        distance: "600 m",
+        rating: 4.3,
+        reviews: 140,
+    },
+    {
+        name: "Kebab Express",
+        distance: "200 m",
+        rating: 4.3,
+        reviews: 140,
+    },
+    {
+        name: "Chef Istanbul",
+        distance: "2.3 km",
+        rating: 4.3,
+        reviews: 140,
+    },
+];
